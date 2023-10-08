af0474a8dfdb1981e71190bf5f54332e93ba6006
thienducthinh <thinhnguyen4410@gmail.com>
2023-10-07 21:03:36 -0700

    Committed

---
+++ initial_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE us_zip_codes (
+    zip SMALLINT PRIMARY KEY,
+    city VARCHAR(50),
+    state_id VARCHAR(2),
+    state_name VARCHAR(50),
+    county_name VARCHAR(50),
+    timezone VARCHAR(50)
+);
+
+
---
+++ uszipcodes.py
@@ -0,0 +1,16 @@
+import csv
+
+# Define the path to your CSV file
+csv_file = 'uszips.csv'  # Update this with your CSV file's path
+insert_template = "INSERT INTO us_zip_codes VALUES ({}, \"{}\", \"{}\", \"{}\", \"{}\", \"{}\");"
+
+# Open the CSV file and process its contents
+with open(csv_file, 'r') as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the header row if it exists
+    for row in reader:
+        zip, city, state_id, state_name, county_name, timezone = row
+        # Generate the INSERT statement
+        insert_statement = insert_template.format(zip, city, state_id, state_name, county_name, timezone)
+        with open('insert_statement.sql', 'a') as file: 
+                file.write(insert_statement + '\n')
